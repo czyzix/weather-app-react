c715a8acc373ad5222e2f95e9e2ec375e9642463
czyzix <czyzevsky@gmail.com>
2023-03-08 13:40:26 +0100

    Added fetching data

--- src/api.js
+++ src/api.js
@@ -8,3 +8,5 @@ export const geoApiOptions  = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "c83b8a140ca1613e6093c1701fa7d1d6"
