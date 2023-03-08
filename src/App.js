55692051c438ec998c01f71090efe3a6f52f3448
czyzix <czyzevsky@gmail.com>
2023-03-08 10:58:17 +0100

    Building city search component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import Search from './components/search/search';
 import './App.css';
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
   return (
     <div className="container">
-      <Search />
+      <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions  = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b2008d7d15mshce0078fda594bb6p16af45jsna78e90852f3f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
